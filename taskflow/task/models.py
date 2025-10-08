8949c150524b076b4682d3489261f696b7f7bbf2
Popeaura <popeahura@gmail.com>
2025-10-08 10:23:32 +0300

    I added my modified projects folder

--- taskflow/task/models.py
+++ taskflow/task/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.model):
+        STATUS_CHOICES = [
+            ('pending','Pending'),
+            ('in_progress','In progress'),
+            ('completed','Completed'),
+        ]
