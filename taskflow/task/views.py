71f108545ec30869418f608f74293defb279cfc1
Popeaura <popeahura@gmail.com>
2025-10-16 18:46:17 +0300

    I added my modified projects folder

--- taskflow/task/views.py
+++ taskflow/task/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks = Task.objects.all()
     return render(request, 'task/task_list.html', {'tasks': tasks})
