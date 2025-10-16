3fe5168d5923378879f6aea3f5d81acd70fe6b49
Popeaura <popeahura@gmail.com>
2025-10-16 18:50:32 +0300

    I added my modified projects folder

--- taskflow/task/admin.py
+++ taskflow/task/admin.py
@@ -3,6 +3,6 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'created_at', 'updated_at')
+    list_display = ('title', 'status', 'priority', 'updated_at')
     list_filter = ('status',)
     search_fields = ('title', 'description')
